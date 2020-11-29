a74b72e042ad0622f1e4d74f8703c4e7cd1643dd
Raphael Vidal <raphael.r.vidal@gmail.com>
2020-11-29 09:37:07 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        //definir tabelas
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        // configura acesso ao banco com o entity
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = localhost; Database = VendasDB; Trusted_Connection = True;");
+        }
+    }
+}
---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+        [Range(1,10,ErrorMessage ="Valor deve ser entre 1 e 10")]
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }// chave estrangeira
+
+        public Categoria Categoria { get; set; } // relação de chave entres as tabelas
+
+    }
+}
